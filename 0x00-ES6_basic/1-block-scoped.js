18d1f72e08d1c94787a60a2b823594411d644fb6
ChaimaeTIBOUDA <125551991+ChaimaeTIBOUDA@users.noreply.github.com>
2024-01-03 15:36:23 +0000

    es6

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,7 @@ export default function taskBlock(trueOrFalse) {
     var task2 = true;
   
     if (trueOrFalse) {
-       task = false;
-       task2 = true;
+       return [task, task2]
     }
   
     return [task, task2];
